8c1e0e94466a5840f67439d2c3b48657d24035e1
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-05-03 17:42:53 +0300

    Doc: articles application added

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
 
+    NEWS_API_ARTICLES_URL ='https://newsapi.org/v2/everything?q=everything&from={}&sortBy=publishedAt&apiKey={}'
     NEWS_API_SOURCES_URL ='https://newsapi.org/v2/sources?apiKey={}'
     NEWS_API_KEY = 'c3011dcec4e546228a335c5326e69378'
     SECRET_KEY = 'charity'
